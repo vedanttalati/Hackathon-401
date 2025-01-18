e93a9bbc7cae2a956242d72b4272c8afc5ffa73f
Nishchay Ranjan <nishchayranjan878@gmail.com>
2025-01-18 13:00:16 -0700

    cc

--- my_project/frontend/401_hackathon/src/App.jsx
+++ my_project/frontend/401_hackathon/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-600 font-bold underline">
+    <h1 className="text-3xl text-blue-500 font-bold underline">
       Hello world!
     </h1>
   )
